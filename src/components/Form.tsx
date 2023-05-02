4a4834bf92def2da71a13019268733497c88ceaa
maximivanov1711 <maxim.ivanov1711@gmail.com>
2023-05-02 08:37:13 +0400

    Added basic form structure.

---
+++ src/components/Form.tsx
@@ -0,0 +1,25 @@
+type Props = {};
+
+const Form = (props: Props) => {
+  return (
+    <form>
+      <div className='mb-3'>
+        <label htmlFor='name' className='form-label'>
+          Name
+        </label>
+        <input type='text' id='name' className='form-control' />
+      </div>
+      <div className='mb-3'>
+        <label htmlFor='age' className='form-label'>
+          Age
+        </label>
+        <input type='number' id='age' className='form-control' />
+      </div>
+      <button type='submit' className='btn btn-primary'>
+        Submit
+      </button>
+    </form>
+  );
+};
+
+export default Form;
